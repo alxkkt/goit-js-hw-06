9cd8f4c4420daf835b2b31c6b2279be0b91a5467
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-10 00:34:34 +0200

    completed second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+// const ingredientItemEl = document.createElement('li');
+// ingredientItemEl.classList.add('item');
+// ingredientItemEl.textContent = ingredient;
+
+const createIngredientEl = function (ingredient) {
+  const ingredientItemEl = document.createElement('li');
+  ingredientItemEl.classList.add('item');
+  ingredientItemEl.textContent = ingredient;
+  
+  return ingredientItemEl;
+}
+
+const createIngredientMarkup = ingredients.map(createIngredientEl);
+ingredientsListRef.append(...createIngredientMarkup);
