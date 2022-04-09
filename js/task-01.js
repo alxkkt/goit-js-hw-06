c0a00abf83da9a323ed01229792ce68f2348b16b
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-10 00:07:47 +0200

    first task done

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,9 @@
-// random change to test git workability
+const categoryItemsRef = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoryItemsRef.length}`);
+
+const elementsRef = document.querySelectorAll('h2');
+elementsRef.forEach(element => {
+    console.log(`Category: ${element.textContent}`);
+    console.log(`Elements: ${element.nextElementSibling.children.length}`)
+})
+
