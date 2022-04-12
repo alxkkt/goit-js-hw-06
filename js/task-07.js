6266f3733de2446164799e43a2f11d110851e0c6
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-12 19:05:10 +0200

    completed seventh task

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const textRef = document.querySelector('#text');
+const inputRef = document.querySelector('#font-size-control');
+
+const onSliderChange = function (event) {
+    textRef.style.fontSize = event.currentTarget.value + 'px';
+}
+
+inputRef.addEventListener('input', onSliderChange);
