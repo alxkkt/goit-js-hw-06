135ecc81107d36bc9651fb9ec25dcfa5d64b0c38
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-12 21:59:31 +0200

    completed ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const buttonRef = body.querySelector('.change-color');
+const textFieldRef = body.querySelector('.color');
+
+
+const onButtonClick = function () {
+  textFieldRef.textContent = getRandomHexColor()
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+
+buttonRef.addEventListener('click', onButtonClick)
