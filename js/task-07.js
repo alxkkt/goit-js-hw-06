a49e0094e6f7a7f0e6036d2ba61a503ca8af43ee
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-12 19:35:59 +0200

    added additional solution to the seventh task

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,8 @@ const textRef = document.querySelector('#text');
 const inputRef = document.querySelector('#font-size-control');
 
 const onSliderChange = function (event) {
-    textRef.style.fontSize = event.currentTarget.value + 'px';
+    // textRef.style.fontSize = event.currentTarget.value + 'px';
+    textRef.style.fontSize = `${event.currentTarget.value}px`
 }
 
 inputRef.addEventListener('input', onSliderChange);
