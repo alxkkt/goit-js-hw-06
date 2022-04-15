babe1fc8d76a066dd7bf70d84f6254c712fdb07e
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-15 16:31:52 +0200

    removed additional DOM operatin

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,13 @@
-const categoryItemsRef = document.querySelectorAll('.item');
+const categoryItemsRef = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoryItemsRef.length}`);
 
-const elementsRef = document.querySelectorAll('h2');
-elementsRef.forEach(element => {
-    console.log(`Category: ${element.textContent}`);
-    console.log(`Elements: ${element.nextElementSibling.children.length}`)
-})
+// const elementsRef = document.querySelectorAll('h2');
+// elementsRef.forEach(element => {
+//     console.log(`Category: ${element.textContent}`);
+//     console.log(`Elements: ${element.nextElementSibling.children.length}`)
+// })
 
+categoryItemsRef.forEach((element) => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
--- js/task-10.js
+++ js/task-10.js
@@ -2,19 +2,17 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const createBtnRef = document.querySelector('[data-create]');
-const destroyBtnRef = document.querySelector('[data-destroy]');
-const containerRef = document.querySelector('#boxes');
+const createBtnRef = document.querySelector("[data-create]");
+const destroyBtnRef = document.querySelector("[data-destroy]");
+const containerRef = document.querySelector("#boxes");
 const inputRef = document.querySelector('[type="number"]');
 
 const createBoxes = function (amount) {
-  containerRef.innerHTML = ''; // добавил, чтобы каждый раз создавалась новая коллекция
-
   let newBoxes = [];
 
   for (let i = 0; i < inputRef.value; i += 1) {
-    containerRef.insertAdjacentHTML('afterbegin', '<div>');
-    newBoxes.push(document.querySelector('#boxes div'));
+    containerRef.insertAdjacentHTML("afterbegin", "<div>");
+    newBoxes.push(document.querySelector("#boxes div"));
   }
 
   newBoxes.forEach((box, index) => {
@@ -22,12 +20,12 @@ const createBoxes = function (amount) {
     box.style.height = `${30 + index * 10}px`;
     box.style.backgroundColor = getRandomHexColor();
   });
-}
+};
 
 const destroyBoxes = function () {
-  containerRef.innerHTML = '';
-  inputRef.value = '';
-}
+  containerRef.innerHTML = "";
+  inputRef.value = "";
+};
 
-createBtnRef.addEventListener('click', createBoxes);
-destroyBtnRef.addEventListener('click', destroyBoxes);
+createBtnRef.addEventListener("click", createBoxes);
+destroyBtnRef.addEventListener("click", destroyBoxes);
