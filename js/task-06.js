9be4260d08c846cf87200dd11b0e1d69b91fe1bc
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-16 01:38:32 +0200

    fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 const inputRef = document.querySelector("#validation-input");
 
 const onInputBlur = function () {
-  inputRef.value.length <= inputRef.dataset.length
-    ? (inputRef.className = "invalid")
-    : (inputRef.className = "valid");
+  inputRef.value.length === inputRef.dataset.length
+    ? (inputRef.className = "valid")
+    : (inputRef.className = "invalid");
 };
 
 inputRef.addEventListener("blur", onInputBlur);
