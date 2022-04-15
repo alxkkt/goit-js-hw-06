6e693f48a88be087bfd6c87b4735ce47ba7f55c1
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-15 16:54:22 +0200

    changed class assignment logic

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
-const inputRef = document.querySelector('#validation-input');
+const inputRef = document.querySelector("#validation-input");
 
-const onInputBlur = function (event) {
-    inputRef.value.length < inputRef.dataset.length ?
-        inputRef.classList.add('invalid') :
-        inputRef.className = 'valid';
-}
+const onInputBlur = function () {
+  inputRef.value.length < inputRef.dataset.length
+    ? (inputRef.className = "invalid")
+    : (inputRef.className = "valid");
+};
 
-inputRef.addEventListener('blur', onInputBlur)
+inputRef.addEventListener("blur", onInputBlur);
